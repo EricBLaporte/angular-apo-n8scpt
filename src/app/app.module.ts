2543c6b4c75f6633812c74c9145f46144ee95528
EricBLaporte <laporte.eric.b@gmail.com>
2020-03-31 09:10:12 +0200

    2 binaries uploaded

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,11 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { MapComponent } from './map/map.component';
+import { TopBarComponent } from './top-bar/top-bar.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent, MapComponent ],
+  declarations: [ AppComponent, HelloComponent, MapComponent, TopBarComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
